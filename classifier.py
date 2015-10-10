ee25e75cf9de6c49087c6ad05a8151888d753bcc
Samuel Taylor <github@samueltaylor.org>
2015-10-09 23:28:17 -0500

    Switch to naive bayes classifier

--- classifier.py
+++ classifier.py
@@ -4,10 +4,10 @@ Usage:
     from classifier import clf
     clf.predict(test_data)
 '''
-from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
 from sklearn.externals import joblib
 
 import asl
 
-clf = svm.SVC(gamma=0.001, C=100.)
+clf = GaussianNB()
 clf.fit(asl.data, asl.target)
